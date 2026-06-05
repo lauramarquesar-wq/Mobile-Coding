66ce41494c5c7700cd062e0f61a9f905e47f7519
laura marques de arandas <lauramarquesar@gmail.com>
2026-06-05 15:42:26 -0300

    Add COLORS constant for theme styling

---
+++ theme.js
@@ -0,0 +1,9 @@
+export const COLORS = {
+  background: '#FEFAFA', 
+  card: '#F8F3EF',       
+  primary: '#EFA9BA',    
+  textDark: '#4A4A4A',   
+  textLight: '#9E9E9E',  
+  green: '#C4D8C4',      
+  border: '#E8DED1',    
+};
